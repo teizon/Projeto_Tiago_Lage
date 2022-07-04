99a3d5e0762f77b84284f7be1ea50c4d01d85631
teizon <79291364+teizon@users.noreply.github.com>
2022-07-04 20:37:27 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,28 @@
+public class A{
+    private int A1;
+    private float A2;
+
+    public int getA1(){
+        return this.A1;
+    }
+
+    public float getA2(){
+        return this.A2;
+    }
+
+    public void setA1(int A1){
+        this.A1 = A1;
+    }
+
+    public void setB2(float A2){
+        this.A2 = A2;
+    }
+
+    public void MA1(){
+        System.out.println(this.A1);
+    }
+
+    public void MA2(){
+        System.out.println(this.A2);
+    }
+}
---
+++ B.java
@@ -0,0 +1,28 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    public int getB1(){
+        return this.B1;
+    }
+
+    public float getB2(){
+        return this.B2;
+    }
+
+    public void setB1(int B1){
+        this.B1 = B1;
+    }
+
+    public void setB2(float B2){
+        this.B2 = B2;
+    }
+
+    public void MB1(){
+        System.out.println(this.B1);
+    }
+
+    public void MB2(){
+        System.out.println(this.B2);
+    }
+}
